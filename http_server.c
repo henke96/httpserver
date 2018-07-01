67c068bf9332255616e2cf1ddcef6d43861d51a3
henke96 <henke96@bredband.net>
2018-07-01 02:43:10 +0200

    Rewrote with epoll

--- http_server.c
+++ http_server.c
@@ -1,11 +1,17 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/epoll.h>
 
 #define HOST_PORT 80
+#define MAX_CONNECTION_QUEUE 128
+#define MAX_EPOLL_EVENTS 64
+#define MAX_CLIENTS 2048
+
 #define RECIEVE_BUFFER_SIZE 1000
 
 void process_request(int client_fd);
@@ -16,17 +22,30 @@ struct http_request_progress {
     int state;
 };
 
+struct Client_socket_info {
+    int fd;
+    char recieve_buffer[RECIEVE_BUFFER_SIZE + 1]; // + 1 so we can add null terminator for printing
+    int recieve_length;
+    int current_line_start_position;
+};
+
+struct epoll_event epoll_events[MAX_EPOLL_EVENTS];
+
+struct Client_socket_info client_socket_infos[MAX_CLIENTS] = {[0 ... MAX_CLIENTS - 1] = {.fd = -1}};
+int client_socket_infos_position = 0;
+
 int main(int argc, char *argv[]) {
-    int listen_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int listen_socket_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
 
     if (listen_socket_fd < 0) {
-        printf("Error creating socket: %s\n", strerror(errno));
+        perror("Error creating socket");
         return 1;
     }
 
     int enable = 1;
     if (setsockopt(listen_socket_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0) {
-        printf("Error setting SO_REUSEADDR option: %s\n", strerror(errno));
+        perror("Error setting SO_REUSEADDR option");
+        return 1;
     }
 
     struct sockaddr_in listen_addr;
@@ -35,68 +54,125 @@ int main(int argc, char *argv[]) {
     listen_addr.sin_port = htons(HOST_PORT);
 
     if (bind(listen_socket_fd, (struct sockaddr *)&listen_addr, sizeof(listen_addr)) < 0) {
-        printf("Error binding socket: %s\n", strerror(errno));
+        perror("Error binding socket");
         return 1;
     }
 
-    if (listen(listen_socket_fd, 3) < 0) {
-        printf("Error listening to socket: %s\n", strerror(errno));
+    if (listen(listen_socket_fd, MAX_CONNECTION_QUEUE) < 0) {
+        perror("Error listening to socket");
+        return 1;
+    }
+
+    int epoll_fd = epoll_create1(0);
+    if (epoll_fd < 0) {
+        perror("Error creating epoll instance");
+        return 1;
+    }
+
+    struct epoll_event listen_socket_event;
+    listen_socket_event.events = EPOLLIN | EPOLLET;
+    listen_socket_event.data.ptr = &listen_socket_fd;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_socket_fd, &listen_socket_event) < 0) {
+        perror("Error registering listen socket fd to epoll");
         return 1;
     }
 
     socklen_t sockaddr_in_size = sizeof(struct sockaddr_in);
     while (1) {
-        int client_socket_fd = accept(listen_socket_fd, (struct sockaddr *)&listen_addr, &sockaddr_in_size);
-        if (client_socket_fd < 0) {
-            printf("Error accepting client socket: %s\n", strerror(errno));
-            continue;
-        }
-
-        process_request(client_socket_fd);
-        close(client_socket_fd);
-    }
-    return 0;
-}
-
-//Process HTTP-request
-void process_request(int client_fd) {
-    struct http_request_progress progress;
-    progress.buffer_position = 0;
-    progress.state = 0;
+        int num_events = epoll_wait(epoll_fd, epoll_events, MAX_EPOLL_EVENTS, -1);
     
-    int recieve_length = 0;
-    while (1) {
-        char recieve_buffer[RECIEVE_BUFFER_SIZE + 1];
-        ssize_t read_size = recv(client_fd, recieve_buffer + recieve_length, RECIEVE_BUFFER_SIZE - recieve_length, 0);
-        printf("%d\n", read_size);
-        if (read_size < 0) {
-            printf("Error recieving message: %s\n", strerror(errno));
-            break;
-        } else if (read_size == 0) {
-            printf("Connection closed or buffer full\n");
-            break;
-        } else {
-            recieve_length += read_size;
-            
-            for (int i = progress.buffer_position; i + 1 < recieve_length;) {
-                if (recieve_buffer[i] == '\r' && recieve_buffer[i + 1] == '\n') {
-                    if (i == progress.buffer_position) {
-                        recieve_buffer[i + 2] = 0;
-                        printf("%s\n", recieve_buffer);
-                        send_test_response(client_fd);
-                        return; 
-                    }
-                    if (i + 2 < recieve_length) {
-                        i += 2;
-                        progress.buffer_position = i;
+        for (int i = 0; i < num_events; ++i) {
+            if (*((int *)epoll_events[i].data.ptr) == listen_socket_fd) {
+                printf("listen socket\n");
+
+                while (1) {
+                    int new_socket_fd = accept4(listen_socket_fd, (struct sockaddr *)&listen_addr, &sockaddr_in_size, SOCK_NONBLOCK);
+                    if (new_socket_fd < 0) {
+                        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                            break;
+                        }
+                        perror("Error accepting client socket");
                         continue;
                     }
-                    break;
+                    printf("Accepted %d\n", new_socket_fd);
+
+                    int start_position = client_socket_infos_position;
+                    while (1) {
+                        struct Client_socket_info *client_socket_info = client_socket_infos + client_socket_infos_position;
+                        ++client_socket_infos_position;
+                        if (client_socket_info->fd == -1) {
+                            struct epoll_event new_socket_event;
+                            new_socket_event.events = EPOLLIN | EPOLLET;
+                            new_socket_event.data.ptr = client_socket_info;
+                            if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_socket_fd, &new_socket_event) < 0) {
+                                perror("Error registering new client socket to epoll");
+                                close(new_socket_fd);
+                                break;
+                            }
+                            client_socket_info->fd = new_socket_fd;
+                            client_socket_info->recieve_length = 0;
+                            client_socket_info->current_line_start_position = 0;
+                            break;
+                        }
+                        if (client_socket_infos_position == start_position) {
+                            printf("Rejected connection due to limit of %d clients reached\n", MAX_CLIENTS);
+                            close(new_socket_fd);
+                            break;
+                        }   
+                    }           
                 }
-                ++i;
+            } else {
+                struct Client_socket_info *client_socket_info = epoll_events[i].data.ptr;
+                int fd = client_socket_info->fd;
+                char *recieve_buffer = client_socket_info->recieve_buffer;
+                printf("event %d\n", fd);
+                while (1) {
+                    size_t read_len = RECIEVE_BUFFER_SIZE - client_socket_info->recieve_length;
+                    if (read_len < 1) {
+                        printf("Closing client since buffer ran out\n");
+                        close(fd);
+                        break;
+                    }
+                    ssize_t read_size = recv(fd, recieve_buffer + client_socket_info->recieve_length, read_len, 0);
+    
+                    if (read_size < 0) {
+                        if (errno != EAGAIN && errno != EWOULDBLOCK) {
+                            perror("Error recieving message from client, closing it");
+                            close(fd);
+                        }
+                        break;
+                    } else if (read_size == 0) {
+                        printf("Recieved 0 bytes, closing client with fd %d\n", fd);
+                        close(fd);
+                        break;
+                    } else {
+                        client_socket_info->recieve_length += read_size;
+                
+                        for (int i = client_socket_info->current_line_start_position; i + 1 < client_socket_info->recieve_length;) {
+                            if (recieve_buffer[i] == '\r' && recieve_buffer[i + 1] == '\n') {
+                                if (client_socket_info->current_line_start_position == i) {
+                                    recieve_buffer[i + 2] = 0;
+                                    printf("%s\n", recieve_buffer);
+                                    send_test_response(fd);
+                                    client_socket_info->recieve_length = 0;
+                                    client_socket_info->current_line_start_position = 0;
+                                    break; 
+                                }
+                                if (i + 2 < client_socket_info->recieve_length) {
+                                    i += 2;
+                                    client_socket_info->current_line_start_position = i;
+                                    continue;
+                                }
+                                break;
+                            }
+                            ++i;
+                        }
+                    }
+                }         
             }
         }
     }
+    return 0;
 }
 
 void send_test_response(int client_fd) {
