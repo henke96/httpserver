e9e3b2b60ef57fce7980d8fed2937b01cfc1234b
henke96 <henke96@bredband.net>
2018-07-11 21:02:12 +0200

    Added url parsing

--- http_server.c
+++ http_server.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -11,22 +12,28 @@
 #define MAX_CONNECTION_QUEUE 128
 #define MAX_EPOLL_EVENTS 64
 #define MAX_CLIENTS 2048
+#define MAX_RESPONSES 2048 // Can (should) be made dynamic
 
 #define RECIEVE_BUFFER_SIZE 1000
 
-void process_request(int client_fd);
-void send_test_response(int client_fd);
+void send_404_response(int client_fd);
+void load_responses();
 
-struct http_request_progress {
-    int buffer_position;
-    int state;
+struct response {
+    char *url;
+    char *response;
+    int response_length;
 };
 
 struct Client_socket_info {
     int fd;
-    char recieve_buffer[RECIEVE_BUFFER_SIZE + 1]; // + 1 so we can add null terminator for printing
-    int recieve_length;
-    int current_line_start_position;
+    char receive_buffer[RECIEVE_BUFFER_SIZE + 1]; // + 1 so we can add null terminator for printing
+    char *receive_end;
+    char *current_line_start;
+    int read_state; // 0: Start, 1: headers, 2: body
+
+    int last_method; // 0: get, 1: other
+    void *last_url_resource; // context given by last_method 
 };
 
 struct epoll_event epoll_events[MAX_EPOLL_EVENTS];
@@ -34,7 +41,12 @@ struct epoll_event epoll_events[MAX_EPOLL_EVENTS];
 struct Client_socket_info client_socket_infos[MAX_CLIENTS] = {[0 ... MAX_CLIENTS - 1] = {.fd = -1}};
 int client_socket_infos_position = 0;
 
+struct response responses[MAX_RESPONSES];
+struct response *responses_end;
+
 int main(int argc, char *argv[]) {
+    load_responses();
+
     int listen_socket_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
 
     if (listen_socket_fd < 0) {
@@ -112,8 +124,11 @@ int main(int argc, char *argv[]) {
                                 break;
                             }
                             client_socket_info->fd = new_socket_fd;
-                            client_socket_info->recieve_length = 0;
-                            client_socket_info->current_line_start_position = 0;
+                            char *receive_buffer = client_socket_info->receive_buffer;
+                            client_socket_info->receive_end = receive_buffer;
+                            client_socket_info->current_line_start = receive_buffer;
+                            client_socket_info->read_state = 0;
+                            client_socket_info->last_url_resource = 0;
                             break;
                         }
                         if (client_socket_infos_position == start_position) {
@@ -126,50 +141,83 @@ int main(int argc, char *argv[]) {
             } else {
                 struct Client_socket_info *client_socket_info = epoll_events[i].data.ptr;
                 int fd = client_socket_info->fd;
-                char *recieve_buffer = client_socket_info->recieve_buffer;
+                char *receive_buffer = client_socket_info->receive_buffer;
                 printf("event %d\n", fd);
                 while (1) {
-                    size_t read_len = RECIEVE_BUFFER_SIZE - client_socket_info->recieve_length;
-                    if (read_len < 1) {
+                    size_t max_read_length = RECIEVE_BUFFER_SIZE + receive_buffer - client_socket_info->receive_end;
+                    if (max_read_length <= 0) {
                         printf("Closing client since buffer ran out\n");
                         close(fd);
                         break;
                     }
-                    ssize_t read_size = recv(fd, recieve_buffer + client_socket_info->recieve_length, read_len, 0);
+                    ssize_t read_length = recv(fd, client_socket_info->receive_end, max_read_length, 0);
     
-                    if (read_size < 0) {
+                    if (read_length < 0) {
                         if (errno != EAGAIN && errno != EWOULDBLOCK) {
                             perror("Error recieving message from client, closing it");
                             client_socket_info->fd = -1;
                             close(fd);
                         }
                         break;
-                    } else if (read_size == 0) {
+                    } else if (read_length == 0) {
                         printf("Recieved 0 bytes, closing client with fd %d\n", fd);
                         client_socket_info->fd = -1;
                         close(fd);
                         break;
                     } else {
-                        client_socket_info->recieve_length += read_size;
-                
-                        for (int i = client_socket_info->current_line_start_position; i + 1 < client_socket_info->recieve_length;) {
-                            if (recieve_buffer[i] == '\r' && recieve_buffer[i + 1] == '\n') {
-                                if (client_socket_info->current_line_start_position == i) {
-                                    recieve_buffer[i + 2] = 0;
-                                    printf("%s\n", recieve_buffer);
-                                    send_test_response(fd);
-                                    client_socket_info->recieve_length = 0;
-                                    client_socket_info->current_line_start_position = 0;
+                        client_socket_info->receive_end += read_length;
+                        
+                        char *read_pos = client_socket_info->current_line_start;
+                        while (read_pos < client_socket_info->receive_end) {
+                            if (*read_pos == '\r' && *(read_pos + 1) == '\n') {
+                                if (read_pos == client_socket_info->current_line_start) { // Line only contained '\r\n'
+                                    *(read_pos + 2) = 0;
+                                    printf("%s\n", receive_buffer);
+                                    if (client_socket_info->last_url_resource == 0) {
+                                        send_404_response(fd);
+                                    } else {
+                                        struct response *response = (struct response *)client_socket_info->last_url_resource;
+                                        send(fd, response->response, response->response_length, 0);
+                                    }
+                                    client_socket_info->receive_end = receive_buffer;
+                                    client_socket_info->current_line_start = receive_buffer;
+                                    client_socket_info->read_state = 0;
+                                    client_socket_info->last_url_resource = 0;
                                     break; 
                                 }
-                                if (i + 2 < client_socket_info->recieve_length) {
-                                    i += 2;
-                                    client_socket_info->current_line_start_position = i;
-                                    continue;
+                                if (client_socket_info->read_state == 0) {
+                                    char *parse_pos = client_socket_info->current_line_start;
+                                    if (*parse_pos == 'G' && *(++parse_pos) == 'E' && *(++parse_pos) == 'T') {
+                                        client_socket_info->last_method = 0; // Last method is GET
+                                        parse_pos += 3; // Skip space and slash
+                                        char *url_start_pos = parse_pos;
+                                        for (struct response *response_pos = responses; response_pos < responses_end; ++response_pos) {
+                                            char *response_url_pos = response_pos->url;
+                                            while (1) {
+                                                char parse_char = *parse_pos;
+                                                char response_url_char = *response_url_pos;
+                                                if (parse_char == response_url_char) {
+                                                    ++parse_pos;
+                                                    ++response_url_pos;
+                                                    continue;
+                                                }
+                                                if (parse_char == ' ' && response_url_char == 0) {
+                                                    client_socket_info->last_url_resource = response_pos;
+                                                    client_socket_info->read_state = 1;
+                                                    goto done_searching_response;
+                                                }
+                                                parse_pos = url_start_pos;
+                                                break;
+                                            }
+                                        }                                                                                
+                                    }
                                 }
-                                break;
+                                done_searching_response:
+                                read_pos += 2;
+                                client_socket_info->current_line_start = read_pos;
+                            } else {
+                                ++read_pos;
                             }
-                            ++i;
                         }
                     }
                 }         
@@ -179,7 +227,14 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void send_test_response(int client_fd) {
-    char *response = "HTTP/1.1 200 OK\r\nContent-Length: 38\r\nContent-Type: text/html\r\n\r\n<html><body>Hello World!</body></html>";
-    send(client_fd, response, strlen(response), 0);
+void load_responses() {
+    responses->url = "test";
+    responses->response = "HTTP/1.1 200 OK\r\nContent-Length:38\r\nContent-Type: text/html\r\n\r\n<html><body>Hello World!</body></html>";
+    responses->response_length = strlen(responses->response);
+    responses_end = responses + 1;
+}
+
+void send_404_response(int client_fd) {
+    char *response = "HTTP/1.1 404 Not Found\r\nContent-Length:0\r\n\r\n";
+    send(client_fd, response, 44, 0);
 }
